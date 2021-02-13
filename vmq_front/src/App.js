7992159d223954d74242a3ae4b58fa9577622b81
22p26c0377 <pawut.j@gmail.com>
2021-02-14 02:21:15 +0800

    test socket IO

--- vmq_front/src/App.js
+++ vmq_front/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import socketIOClient from 'socket.io-client'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      input: '',
+      message: [],
+      endpoint: "http://localhost:9000" // เชื่อมต่อไปยัง url ของ realtime server
+    }
+  }
+
+  componentDidMount = () => {
+    this.response()
+  }
+
+  // เมื่อมีการส่งข้อมูลไปยัง server
+  send = (message) => {
+    const { endpoint, input } = this.state
+    const socket = socketIOClient(endpoint)
+    socket.emit('sent-message', input)
+    this.setState({ input: '' })
+  }
+
+  // รอรับข้อมูลเมื่อ server มีการ update
+  response = () => {
+    const { endpoint, message } = this.state
+    const temp = message
+    const socket = socketIOClient(endpoint)
+    socket.on('new-message', (messageNew) => {
+      temp.push(messageNew)
+      this.setState({ message: temp })
+    })
+  }
+
+  changeInput = (e) => {
+    this.setState({ input: e.target.value })
+  }
+
+  render() {
+    const { input, message } = this.state
+    return (
+      <div>
+        <div style={style}>
+          <input value={input} onChange={this.changeInput} />
+          <button onClick={() => this.send()}>Send</button>
+        </div>
+        {
+          message.map((data, i) =>
+            <div key={i} style={style} >
+              {i + 1} : {data}
+            </div>
+          )
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+const style = { marginTop: 20, paddingLeft: 50 }
+
+export default App
