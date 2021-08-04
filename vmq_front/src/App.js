43bbeefd941e4064c5b9020ca6af446537145c0b
22p26c0377 <pawut.j@gmail.com>
2021-08-05 06:20:08 +0800

    Add sentMessage

--- vmq_front/src/App.js
+++ vmq_front/src/App.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:4001";
+const data = { foo: 1, bar: 2 };
 
 function App() {
   const [response, setResponse] = useState("");
 
+  const sendMessage = (message) => {
+    fetch(`${ENDPOINT}/update/?message="test"`, {
+      method: "GET",
+    });
+  };
+
   useEffect(() => {
-   
     const socket = socketIOClient(ENDPOINT);
-    socket.on("FromAPI", data => {
-      console.log("UPDATE")
+    socket.on("FromAPI", (data) => {
+      console.log("UPDATE");
+      console.log(data);
       setResponse(data);
     });
   }, []);
 
   return (
-    <p>
-      It's <time dateTime={response}>{response}</time>
-    </p>
+    <div>
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
+      <button onClick={sendMessage}>test Emit</button>
+    </div>
   );
 }
 
