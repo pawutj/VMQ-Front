36aae762a1a850794feba8bca07495f71139cb71
22p26c0377 <pawut.j@gmail.com>
2021-08-07 06:03:28 +0800

    Add Endcredit Component

---
+++ vmq_front/src/EndCredit.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+function EndCredit() {
+  return (
+    <div>
+      <h1> Thank you for participating. </h1>
+      <h2> If You found bug. Report https://github.com/pawutj/VMQ-Front </h2>
+    </div>
+  );
+}
+
+export default EndCredit;
