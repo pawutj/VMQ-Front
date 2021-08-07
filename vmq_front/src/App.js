fcd9326d4478b44d0e0e37050b626850a59554e4
22p26c0377 <pawut.j@gmail.com>
2021-08-07 17:50:13 +0800

    Fix Endpoint

--- vmq_front/src/App.js
+++ vmq_front/src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import EndCredit from "./EndCredit";
 import socketIOClient from "socket.io-client";
 import Content from "./Content";
-const ENDPOINT = "http://127.0.0.1:4001";
-//const ENDPOINT = "http://159.138.246.162:4001";
+//const ENDPOINT = "http://127.0.0.1:4001";
+const ENDPOINT = "http://159.138.246.162:4001";
 
 const data = { foo: 1, bar: 2 };
 
