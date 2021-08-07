e90472c14a40a70ca05b9964b16e248ccc9e90c7
22p26c0377 <pawut.j@gmail.com>
2021-08-07 19:08:35 +0800

    Add ChoicePage

--- vmq_front/src/ChoicePage.js
+++ vmq_front/src/ChoicePage.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { shuffle } from "./util";
+import { shuffle, getRandomNFromArray } from "./util";
+import { _mockData, _mockAllTitle } from "./mock";
 
+const getChoice = () => [
+  ...getRandomNFromArray(_mockData, 5),
+  { title: "None", url: "None" },
+];
 function ChoicePage() {
+  const [randomChoice, setRandomChoice] = useState(getChoice());
+  const random = (c) => {
+    setRandomChoice(getChoice());
+    console.log(c);
+  };
   return (
     <div>
       <h1>test</h1>
+      {randomChoice.map((c) => (
+        <button style={{ margin: 10 }} onClick={() => random(c)}>
+          <h4>{c.title}</h4>
+        </button>
+      ))}
     </div>
   );
 }
