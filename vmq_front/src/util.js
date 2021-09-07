5f8f332807cd21343d20293b49f7c700a7203e29
22p26c0377 <pawut.j@gmail.com>
2021-09-08 02:28:57 +0800

    Fix Array

--- vmq_front/src/util.js
+++ vmq_front/src/util.js
@@ -31,27 +31,36 @@ const filterByVote = (t) => {
   };
 };
 
-function shuffle(array) {
-  var currentIndex = array.length,
-    randomIndex;
+// function shuffle(array) {
+//   var currentIndex = array.length,
+//     randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
+//   // While there remain elements to shuffle...
+//   while (0 !== currentIndex) {
+//     // Pick a remaining element...
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex--;
 
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
+//     // And swap it with the current element.
+//     [array[currentIndex], array[randomIndex]] = [
+//       array[randomIndex],
+//       array[currentIndex],
+//     ];
+//   }
+
+//   return array;
+// }
+
+const shuffleArray = (array) => {
+  let _array;
+  for (let i = _array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [_array[i], _array[j]] = [_array[j], _array[i]];
   }
-
   return array;
-}
+};
 
-const getRandomNFromArray = (array, n) => shuffle(array).slice(0, n);
+const getRandomNFromArray = (array, n) => shuffleArray(array).slice(0, n);
 
 export {
   getRandomNFromArray,
